3ce3921140513e683c4b3b35e3b9cd7251ff480d
Shakir Al Hasan <shakiritcse@gmail.com>
2025-03-02 20:17:42 +0600

    final task

--- script/date.js
+++ script/date.js
@@ -1,4 +1,4 @@
 const dateElement = document.getElementById("date");
  const options = { weekday: "short", month: "short", day: "2-digit", year: "numeric" };
 const today = new Date().toLocaleDateString("en-US",options);
-dateElement.innerHTML = today.replace(",","");
+dateElement.innerHTML = today.replace(',','');
